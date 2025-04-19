de7aee06d77bf21b7769ef29863727832e4b9cc3
sam lee <leesamuel423@gmail.com>
2025-04-19 14:47:48 -0400

    admin: initialization

---
+++ cmd/interview-cli/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// seed random number generator
+
+	// command line flags
+
+	// initial configurations
+
+	// initialize question repository
+
+	// switch cases based off which command
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module interview-cli
+
+go 1.23.4
