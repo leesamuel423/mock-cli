4b092aaf43b1551da49af2e16d40d12a883d6665
sam lee <leesamuel423@gmail.com>
2025-04-19 21:37:40 -0400

    feat: configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,80 @@
 package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// application configuration ... stores location of the questions file
+type Config struct {
+	QuestionsFile string `json:"questionsFile"` // path to file
+}
+
+// load config from disk; if no config, create default config
+// returns pointer to config and error
+func LoadConfig() (*Config, error) {
+	// get user's home directory
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get home directory: %w", err)
+	}
+
+	// construct path to app config directory
+	configDir := filepath.Join(homeDir, ".interview-cli")
+
+	// create config directory if doesn't exist
+	if err := os.MkdirAll(configDir, 0755); err != nil {
+		return nil, fmt.Errorf("failed to create config directory: %w", err)
+	}
+
+	configPath := filepath.Join(configDir, "config.json")
+
+	// init default config structure
+	config := &Config{
+		QuestionsFile: filepath.Join(configDir, "questions.json"),
+	}
+
+	// check if config file exists
+	if _, err := os.Stat(configPath); err == nil {
+		// read and parse JSON
+		data, err := os.ReadFile(configPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse config JSON: %w", err)
+		}
+
+		if err := json.Unmarshal(data, &config); err != nil {
+			return nil, fmt.Errorf("failed to parse config JSON: %w", err)
+		}
+	} else { // if config file doesn't exist, create w/ default values
+		data, err := json.MarshalIndent(config, "", " ")
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal config to JSON: %w", err)
+		}
+
+		// write default config to disk
+		if err := os.WriteFile(configPath, data, 0644); err != nil {
+			return nil, fmt.Errorf("failed to write config to file: %w", err)
+		}
+	}
+
+	// check if questions file exists
+	if _, err := os.Stat(config.QuestionsFile); os.IsNotExist(err) {
+		emptyQuestions := []any{}
+
+		// convert empty slice to formatted JSON
+		data, err := json.MarshalIndent(emptyQuestions, "", " ")
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal empty questions to JSON: %w", err)
+		}
+
+		// write empty question array to disk
+		if err := os.WriteFile(config.QuestionsFile, data, 0644); err != nil {
+			return nil, fmt.Errorf("failed to create questions file: %w", err)
+		}
+	}
+
+	// return loaded or created config
+	return config, nil
+}
